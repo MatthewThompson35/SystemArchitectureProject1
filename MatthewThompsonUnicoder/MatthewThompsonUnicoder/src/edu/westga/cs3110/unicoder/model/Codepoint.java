c0850f148d7a3396193e0a93119a5754bb5de832
Matthew Thompson <93340660+MatthewThompson35@users.noreply.github.com>
2022-09-17 16:51:16 -0400

    Delete MatthewThompsonUnicoder/MatthewThompsonUnicoder directory

--- MatthewThompsonUnicoder/MatthewThompsonUnicoder/src/edu/westga/cs3110/unicoder/model/Codepoint.java
+++
@@ -1,5 +0,0 @@
-package edu.westga.cs3110.unicoder.model;
-
-public class Codepoint {
-
-}
--- MatthewThompsonUnicoder/MatthewThompsonUnicoder/src/edu/westga/cs3110/unicoder/tests/model/codepoint/TestConstructor.java
+++
@@ -1,5 +0,0 @@
-package edu.westga.cs3110.unicoder.tests.model.codepoint;
-
-public class TestConstructor {
-
-}
