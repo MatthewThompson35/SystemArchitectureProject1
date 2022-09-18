5cfa3d02f9ac1d463b9823e1b112bde7476d61d7
Matthew Thompson <mthomp35@my.westga.edu>
2022-09-18 15:05:03 -0400

    Added toUTF32() method and its tests

--- MatthewThompsonUnicoder/src/edu/westga/cs3110/unicoder/model/Codepoint.java
+++ MatthewThompsonUnicoder/src/edu/westga/cs3110/unicoder/model/Codepoint.java
@@ -1,5 +1,6 @@
 package edu.westga.cs3110.unicoder.model;
 
+
 public class Codepoint {
 	String codepoint;
 	
@@ -10,5 +11,40 @@ public class Codepoint {
 	public String getCodepoint() {
 		return this.codepoint;
 	}
+	
+	public String toUTF32() {
+		String result = "0x";
+		result += ("00000000" + this.codepoint.substring(2)).substring(this.codepoint.substring(2).length());
+		return result;
+		
+	}
+	
+	public String toUTF16() {
+		return this.codepoint;
+	}
+	
+	public String toUTF8() {
+		return this.codepoint;
+	}
+	
+	private String hexToBin(String hex){
+        hex = hex.replaceAll("0", "0000");
+        hex = hex.replaceAll("1", "0001");
+        hex = hex.replaceAll("2", "0010");
+        hex = hex.replaceAll("3", "0011");
+        hex = hex.replaceAll("4", "0100");
+        hex = hex.replaceAll("5", "0101");
+        hex = hex.replaceAll("6", "0110");
+        hex = hex.replaceAll("7", "0111");
+        hex = hex.replaceAll("8", "1000");
+        hex = hex.replaceAll("9", "1001");
+        hex = hex.replaceAll("A", "1010");
+        hex = hex.replaceAll("B", "1011");
+        hex = hex.replaceAll("C", "1100");
+        hex = hex.replaceAll("D", "1101");
+        hex = hex.replaceAll("E", "1110");
+        hex = hex.replaceAll("F", "1111");
+        return hex;
+    }
 
 }
--- MatthewThompsonUnicoder/src/edu/westga/cs3110/unicoder/tests/codepoint/TestToUTF32.java
+++ MatthewThompsonUnicoder/src/edu/westga/cs3110/unicoder/tests/codepoint/TestToUTF32.java
@@ -1,18 +1,17 @@
 package edu.westga.cs3110.unicoder.tests.codepoint;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import edu.westga.cs3110.unicoder.model.Codepoint;
 
-public class TestConstructor {
+class TestToUTF32 {
 
 	@Test
 	public void testConstructor() {
 		Codepoint codepoint = new Codepoint();
-		assertEquals("0x1F682", codepoint.getCodepoint());
+		assertTrue("0x0001F682".equalsIgnoreCase(codepoint.toUTF32()));
 	}
-	
 
 }
